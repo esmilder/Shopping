c2084ad0a7c8700c65e8af69d9a8fe713e16184c
Esmilder Lopez <esmilder10.lp@gmail.com>
2022-04-28 14:17:31 -0600

    Creacion de CRUD Country

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="País")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener {1} carácteres como mínimo")]
+        [Required (ErrorMessage ="El campo {0} es obligatorio")]
+        public string? NameCountry { get; set; }
+
+    }
+}
